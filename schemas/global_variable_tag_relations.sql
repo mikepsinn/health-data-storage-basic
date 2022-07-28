c3e42f0fbf73ae9a23667bcc936a1f84a0434acf
Mike P. Sinn <m@quantimodo.com>
2022-07-28 14:01:59 -0500

    Update global_variable_tag_relations.sql

--- schemas/global_variable_tag_relations.sql
+++ schemas/global_variable_tag_relations.sql
@@ -2,11 +2,17 @@ create table global_variable_tag_relations
 (
     global_variable_tag_id integer
         constraint global_variable_tag_relations_global_variable_tags_id_fk
-            references global_variable_tags,
+            references qm_production.global_variable_tags,
     global_variable_id     bigint
         constraint global_variable_tag_relations_global_variables_id_fk
-            references global_variables
+            references qm_production.global_variables
 );
 
-alter table global_variable_tag_relations
-    owner to postgres;
+comment on table global_variable_tag_relations is 'Defines the tags assigned to  each variable';
+
+comment on column global_variable_tag_relations.global_variable_tag_id is 'The related tag id';
+
+comment on column global_variable_tag_relations.global_variable_id is 'The related variable id
+';
+
+
